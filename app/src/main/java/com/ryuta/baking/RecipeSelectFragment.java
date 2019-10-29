540efbb28682e3f9132e9739574dddc6ec18911f
Miles Chew <mileschew42@gmail.com>
2019-10-28 18:44:17 -0700

    add main fragments

---
+++ app/src/main/java/com/ryuta/baking/RecipeSelectFragment.java
@@ -0,0 +1,28 @@
+package com.ryuta.baking;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class RecipeSelectFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipe_select, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public static RecipeSelectFragment newInstance() {
+        return new RecipeSelectFragment();
+    }
+}
---
+++ app/src/main/java/com/ryuta/baking/StepDetailFragment.java
@@ -0,0 +1,28 @@
+package com.ryuta.baking;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class StepDetailFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_step_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public static StepDetailFragment newInstance() {
+        return new StepDetailFragment();
+    }
+}
