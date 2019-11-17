0044d1d563561789bc7b465bf789ab0be769eba7
Miles Chew <mileschew42@gmail.com>
2019-11-16 22:38:46 -0800

    getters and setters

--- app/src/main/java/com/ryuta/baking/models/Recipe.java
+++ app/src/main/java/com/ryuta/baking/models/Recipe.java
@@ -7,4 +7,52 @@ public class Recipe {
     private Step[] steps;
     private int servings;
     private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Step[] getSteps() {
+        return steps;
+    }
+
+    public void setSteps(Step[] steps) {
+        this.steps = steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
